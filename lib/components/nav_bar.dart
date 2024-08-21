825557b68d22045d33fe8040e977104b2806286e
kareraolivier <kareraolivier@gmail.com>
2024-08-21 09:58:07 +0200

    feat: add routing

--- lib/components/nav_bar.dart
+++ lib/components/nav_bar.dart
@@ -6,23 +6,24 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-        ],
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              Navigator.pushNamed(context, './home');
-              break;
-            case 1:
-              Navigator.pushNamed(context, './about');
-              break;
-            case 2:
-              Navigator.pushNamed(context, './profile');
-              break;
-          }
-        });
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+      ],
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            Navigator.pushNamed(context, '/home');
+            break;
+          case 1:
+            Navigator.pushNamed(context, '/about');
+            break;
+          case 2:
+            Navigator.pushNamed(context, '/profile');
+            break;
+        }
+      },
+    );
   }
 }
